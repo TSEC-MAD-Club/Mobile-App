fd6272c670abcf525f5c9c60f34a4a911fcd6aae
SirusCodes <darshandrander@gmail.com>
2021-08-19 15:59:00 +0530

    feat: add app bar

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+import 'screens/committees_screen.dart';
 import 'utils/themes.dart';
 
 void main() {
@@ -16,58 +17,7 @@ class TSECApp extends StatelessWidget {
       themeMode: ThemeMode.light,
       theme: theme,
       darkTheme: darkTheme,
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-            TextButton(
-              onPressed: () {},
-              child: const Text("Delete assignment"),
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
+      home: const CommittessScreen(),
     );
   }
 }
---
+++ lib/utils/image_assets.dart
@@ -0,0 +1,8 @@
+class ImageAssets {
+  ImageAssets._();
+
+  static const _root = "assets/images";
+
+  // Appbar images
+  static const committes = "$_root/app_bar/committes.png";
+}
