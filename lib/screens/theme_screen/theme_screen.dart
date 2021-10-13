7edd0e51e6ecd3640fcb6d2a6aa3d74f3e1e8f33
Tanay Kamath <2tanaykamath@gmail.com>
2021-10-13 15:40:39 +0530

    rm: redundant Body widget from theme_screen.dart

--- lib/screens/theme_screen/theme_screen.dart
+++ lib/screens/theme_screen/theme_screen.dart
@@ -6,63 +6,56 @@ import 'widgets/custom_toggle_button.dart';
 import 'widgets/skip_and_next_row.dart';
 import '../../utils/image_assets.dart';
 
-class ThemeScreen extends StatelessWidget {
+class ThemeScreen extends StatefulWidget {
   const ThemeScreen({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Body(),
-    );
-  }
+  _ThemeScreenState createState() => _ThemeScreenState();
 }
 
-class Body extends StatefulWidget {
-  const Body({Key? key}) : super(key: key);
-
-  @override
-  _BodyState createState() => _BodyState();
-}
-
-class _BodyState extends State<Body> {
+class _ThemeScreenState extends State<ThemeScreen> {
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
 
-    return Container(
-      height: size.height,
-      width: size.width,
-      child: Column(
-        children: <Widget>[
-          const SizedBox(height: 100),
-          Consumer(builder: (context, watch, child) {
-            final themeMode = watch(themeProvider);
-            return Image.asset(
-              themeMode==ThemeMode.dark ? ImageAssets.moon : ImageAssets.sun,
-              height: 0.25 * size.height,
-              width: 0.54 * size.width,
-            );
-          }),
-          const SizedBox(height: 40),
-          Text(
-            "Choose a style",
-            style: Theme.of(context).textTheme.headline3,
-          ),
-          const SizedBox(height: 15),
-          Text(
-            "Pop or subtle. Day or night. \n Customize your interface.",
-            style: Theme.of(context).textTheme.subtitle1,
-          ),
-          SizedBox(height: 0.05 * size.height),
-          CustomToggleButton(
-            values: const ['Light', 'Dark'],
-            onToggleCallback: (index) {
-              context.read(themeProvider.notifier).switchTheme();
-            },
-          ),
-          const Spacer(),
-          const SkipAndNextRow(),
-        ],
+    return Scaffold(
+      body: Container(
+        height: size.height,
+        width: size.width,
+        child: Column(
+          children: <Widget>[
+            const SizedBox(height: 100),
+            Consumer(builder: (context, watch, child) {
+              final themeMode = watch(themeProvider);
+              return Image.asset(
+                themeMode == ThemeMode.dark
+                    ? ImageAssets.moon
+                    : ImageAssets.sun,
+                height: 0.25 * size.height,
+                width: 0.54 * size.width,
+              );
+            }),
+            const SizedBox(height: 40),
+            Text(
+              "Choose a style",
+              style: Theme.of(context).textTheme.headline3,
+            ),
+            const SizedBox(height: 15),
+            Text(
+              "Pop or subtle. Day or night. \n Customize your interface.",
+              style: Theme.of(context).textTheme.subtitle1,
+            ),
+            SizedBox(height: 0.05 * size.height),
+            CustomToggleButton(
+              values: const ['Light', 'Dark'],
+              onToggleCallback: (index) {
+                context.read(themeProvider.notifier).switchTheme();
+              },
+            ),
+            const Spacer(),
+            const SkipAndNextRow(),
+          ],
+        ),
       ),
     );
   }
