7f2cfa210290058e27d6bbe7a2ce6886187b075f
SirusCodes <darshandrander@gmail.com>
2022-01-10 14:02:45 +0530

    feat: add download file functionality

--- lib/screens/notification_screen/widgets/notification_dialog.dart
+++ lib/screens/notification_screen/widgets/notification_dialog.dart
@@ -1,6 +1,10 @@
-import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
-import 'package:tsec_app/models/notification_model/notification_model.dart';
+import 'package:flutter/scheduler.dart';
+import 'package:open_file/open_file.dart';
+
+import '../../../models/notification_model/notification_model.dart';
+import '../../../utils/init_get_it.dart';
+import '../../../utils/storage_util.dart';
 
 class NotificationDialog extends StatelessWidget {
   const NotificationDialog({
@@ -34,31 +38,90 @@ class NotificationDialog extends StatelessWidget {
   }
 
   List<Widget> _buildDownloadButtons(List<String> urls) {
-    final _storage = FirebaseStorage.instance;
-    return urls.map(
-      (url) {
-        final _ref = _storage.refFromURL(url);
-        return Padding(
-          padding: const EdgeInsets.only(top: 5),
-          child: ElevatedButton(
-            style: ElevatedButton.styleFrom(onPrimary: Colors.white),
-            onPressed: () {},
-            child: Row(
-              children: <Widget>[
-                const Icon(Icons.download),
-                Expanded(
-                  child: Text(
-                    _ref.name,
-                    textAlign: TextAlign.center,
-                    maxLines: 1,
-                    overflow: TextOverflow.ellipsis,
-                  ),
+    return urls.map((url) => _DownloadButton(url: url)).toList();
+  }
+}
+
+class _DownloadButton extends StatefulWidget {
+  const _DownloadButton({
+    Key? key,
+    required this.url,
+  }) : super(key: key);
+
+  final String url;
+
+  @override
+  State<_DownloadButton> createState() => _DownloadButtonState();
+}
+
+class _DownloadButtonState extends State<_DownloadButton> {
+  late final StorageUtil _storage;
+  StorageResult? _storageResult;
+  double _downloadPrecent = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    SchedulerBinding.instance?.addPostFrameCallback((_) {
+      _storage = locator<StorageUtil>();
+      _storage
+          .getResult(widget.url)
+          .then((value) => setState(() => _storageResult = value));
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(top: 5),
+      child: ElevatedButton(
+        style: ElevatedButton.styleFrom(onPrimary: Colors.white),
+        onPressed: _storageResult == null ? null : _onButtonClick,
+        child: _storageResult == null || _storageResult!.isDownloadInProgress
+            ? SizedBox(
+                height: 24,
+                width: 24,
+                child: CircularProgressIndicator(
+                  value: _downloadPrecent <= 0 ? null : _downloadPrecent,
                 ),
-              ],
-            ),
-          ),
-        );
+              )
+            : Row(
+                children: <Widget>[
+                  _storageResult!.path != null
+                      ? const Icon(Icons.open_in_new)
+                      : const Icon(Icons.download),
+                  Expanded(
+                    child: Text(
+                      _storageResult!.name,
+                      textAlign: TextAlign.center,
+                      maxLines: 1,
+                      overflow: TextOverflow.ellipsis,
+                    ),
+                  ),
+                ],
+              ),
+      ),
+    );
+  }
+
+  void _onButtonClick() {
+    if (_storageResult!.path != null) {
+      OpenFile.open(_storageResult!.path!, type: _storageResult!.type);
+      return;
+    }
+    _storageResult = _storageResult!.updateDownloadStatus(status: true);
+    _storage.downloadFile(
+      result: _storageResult!,
+      progress: (percentage, path) {
+        setState(() {
+          _downloadPrecent = percentage;
+          if (_downloadPrecent == 1.0)
+            _storageResult = _storageResult!.updateDownloadStatus(
+              status: false,
+              path: path,
+            );
+        });
       },
-    ).toList();
+    );
   }
 }
--- lib/utils/init_get_it.dart
+++ lib/utils/init_get_it.dart
@@ -1,8 +1,10 @@
 import 'package:get_it/get_it.dart';
 import 'package:tsec_app/services/notification_service.dart';
+import 'package:tsec_app/utils/storage_util.dart';
 
 final locator = GetIt.instance;
 
 void initGetIt() {
   locator.registerLazySingleton(() => NotificationService());
+  locator.registerLazySingleton(() => StorageUtil());
 }
