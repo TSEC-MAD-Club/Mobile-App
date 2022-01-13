a2ed1fb5c9b6554f8de284fe7f2e974bf98fce04
SirusCodes <darshandrander@gmail.com>
2022-01-13 23:57:14 +0530

    feat: integrate fcm

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,22 @@
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-import 'package:tsec_app/utils/init_get_it.dart';
 
 import 'firebase_options.dart';
+import 'models/notification_model/notification_model.dart';
 import 'provider/shared_prefs_provider.dart';
 import 'provider/theme_provider.dart';
 import 'screens/notification_screen/notification_screen.dart';
+import 'screens/notification_screen/widgets/notification_dialog.dart';
+import 'utils/init_get_it.dart';
+import 'utils/notification_type.dart';
 import 'utils/themes.dart';
+
+// To handle all the background messages
+// Currently not used but wont work if not present
+Future<void> _handleBackgroundMessage(RemoteMessage message) async {}
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -16,6 +24,8 @@ Future<void> main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+
+  FirebaseMessaging.onBackgroundMessage(_handleBackgroundMessage);
 
   initGetIt();
 
@@ -31,11 +41,22 @@ Future<void> main() async {
   );
 }
 
-class TSECApp extends ConsumerWidget {
+class TSECApp extends ConsumerStatefulWidget {
   const TSECApp({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  ConsumerState<ConsumerStatefulWidget> createState() => _TSECAppState();
+}
+
+class _TSECAppState extends ConsumerState<TSECApp> {
+  @override
+  void initState() {
+    super.initState();
+    _setupFCMNotifications();
+  }
+
+  @override
+  Widget build(BuildContext context) {
     final _themeMode = ref.watch(themeProvider);
     return MaterialApp(
       title: 'TSEC App',
@@ -44,5 +65,59 @@ class TSECApp extends ConsumerWidget {
       darkTheme: darkTheme,
       home: const NotificationScreen(),
     );
+  }
+
+  Future<void> _setupFCMNotifications() async {
+    final _messaging = FirebaseMessaging.instance;
+    final _permission = await _messaging.requestPermission(provisional: true);
+
+    if ([
+      AuthorizationStatus.authorized,
+      AuthorizationStatus.provisional,
+    ].contains(_permission.authorizationStatus)) {
+      _messaging.subscribeToTopic(NotificationType.notification);
+      _setupInteractedMessage();
+      _messageOnForeground();
+    }
+  }
+
+  void _messageOnForeground() {
+    FirebaseMessaging.onMessage.listen(_handleMessage);
+  }
+
+  Future<void> _setupInteractedMessage() async {
+    // Get any messages which caused the application to open from
+    // a terminated state.
+    RemoteMessage? initialMessage =
+        await FirebaseMessaging.instance.getInitialMessage();
+
+    if (initialMessage != null) {
+      _handleMessage(initialMessage);
+    }
+
+    // Also handle any interaction when the app is in the background via a
+    // Stream listener
+    FirebaseMessaging.onMessageOpenedApp.listen(_handleMessage);
+  }
+
+  void _handleMessage(RemoteMessage message) {
+    // from - if message is sent from notification topic
+    if (message.from == NotificationType.notification.addTopicsPrefix) {
+      showDialog(
+        context: context,
+        builder: (context) => NotificationDialog(
+          notificationModel: NotificationModel(
+            message: message.notification!.body!,
+            title: message.notification!.title!,
+            notificationTime: DateTime.parse(
+              message.data["notificationTime"] ?? DateTime.now().toString(),
+            ),
+            attachments: (message.data["attachments"] as List?)
+                ?.map((e) => e as String)
+                .toList(),
+          ),
+        ),
+      );
+    }
   }
 }
