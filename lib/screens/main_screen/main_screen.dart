3d4c440779bf830715f7b7e875ed469a690ef37f
Krishanadave671 <84977709+Krishanadave671@users.noreply.github.com>
2022-09-03 12:47:50 +0530

    remove unused imports

--- lib/screens/main_screen/main_screen.dart
+++ lib/screens/main_screen/main_screen.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
-
-import '../../provider/theme_provider.dart';
 import '../../utils/department_enum.dart';
 import '../../utils/image_assets.dart';
 import '../../utils/launch_url.dart';
--- lib/widgets/custom_app_bar.dart
+++ lib/widgets/custom_app_bar.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
-
-import '../provider/theme_provider.dart';
 import '../utils/themes.dart';
 
 class CustomAppBar extends ConsumerWidget with PreferredSizeWidget {
