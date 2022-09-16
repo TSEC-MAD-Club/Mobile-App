d8a16ac802a4f230880d3c2bb19ff7de2e4d23a0
AbhinayPatil <10abhinay10@gmail.com>
2022-09-16 22:09:29 +0530

    add const and use switch case in departmnet_enum

--- lib/utils/department_enum.dart
+++ lib/utils/department_enum.dart
@@ -1,24 +1,32 @@
-enum DepartmentEnum { aids, cs, it, biomed, biotech, chem, extc, fe }
+enum DepartmentEnum {
+  aids,
+  cs,
+  it,
+  biomed,
+  biotech,
+  chem,
+  extc,
+  fe;
 
-const _departmentName = {
-  DepartmentEnum.aids: "AI & DS",
-  DepartmentEnum.biomed: "Biomedical Engineering",
-  DepartmentEnum.biotech: "Bio-Tech Engineering",
-  DepartmentEnum.chem: "Chemical Engineering",
-  DepartmentEnum.cs: "Computer Engineering",
-  DepartmentEnum.extc: "EXTC",
-  DepartmentEnum.fe: "First Year",
-  DepartmentEnum.it: "Information Technology",
-};
-
-extension GetDepartmentName on DepartmentEnum {
-  String get getName {
-    assert(
-      DepartmentEnum.values.length == 8,
-      "Add department name in the map as well",
-    );
-
-    return _departmentName[this]!;
+  String get name {
+    switch (this) {
+      case DepartmentEnum.aids:
+        return "AI & DS";
+      case DepartmentEnum.cs:
+        return "Computer Engineering";
+      case DepartmentEnum.it:
+        return "Information Technology";
+      case DepartmentEnum.biomed:
+        return "Biomedical Engineering";
+      case DepartmentEnum.biotech:
+        return "Bio-Tech Engineering";
+      case DepartmentEnum.chem:
+        return "Chemical Engineering";
+      case DepartmentEnum.extc:
+        return "EXTC";
+      case DepartmentEnum.fe:
+        return "First Year";
+    }
   }
 
   String get fileName {
