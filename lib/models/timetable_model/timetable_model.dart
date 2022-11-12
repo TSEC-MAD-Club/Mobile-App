5e5be1aa75013b506a137f4d77458e1370279365
Krishanadave671 <84977709+Krishanadave671@users.noreply.github.com>
2022-11-12 13:16:21 +0530

    model class created

---
+++ lib/models/timetable_model/timetable_model.dart
@@ -0,0 +1,26 @@
+import 'package:json_annotation/json_annotation.dart';
+part 'timetable_model.g.dart';
+
+@JsonSerializable()
+class TimetableModel {
+  final String lectureName;
+  final String lectureStartTime;
+  final String lectureEndTime;
+  final String lectureFacultyImageurl;
+  final String lectureFacultyName;
+  final String lectureBatch; 
+
+  TimetableModel({
+    required this.lectureName,
+    required this.lectureStartTime,
+    required this.lectureEndTime,
+    required this.lectureFacultyImageurl,
+    required this.lectureFacultyName,
+    required this.lectureBatch,
+  });
+
+  factory TimetableModel.fromJson(Map<String, dynamic> json) =>
+      _$TimetableModelFromJson(json);
+    
+  Map<String, dynamic> toJson() => _$TimetableModelToJson(this);
+}
