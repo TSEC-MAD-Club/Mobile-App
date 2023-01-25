9fa507dd4497687df6bc4006c6ba3b3662e93ab1
prasaddd77 <prasadranjane2304@gmail.com>
2023-01-26 01:44:11 +0530

    Faculty and Committee Images

---
+++ lib/utils/faculty_details.dart
@@ -0,0 +1,288 @@
+String getFacultyImagebyName(String facultyName) {
+  switch (facultyName) {
+    case "Aejaz Khan":
+      return "https://firebasestorage.googleapis.com/v0/b/tsec-app.appspot.com/o/faculty%2Fcomps%2Faejazkhan.jpg?alt=media&token=ed466411-9439-4096-ae72-78d894e86305";
+
+    case "Anagha Durugkar":
+      return "https://firebasestorage.googleapis.com/v0/b/tsec-app.appspot.com/o/faculty%2Fcomps%2Fanaghadurugkar.jpg?alt=media&token=9b3754df-6ae5-4400-9b51-d6eefd9cfa94";
+
+    case "Anil Chhangani":
+      return "https://firebasestorage.googleapis.com/v0/b/tsec-app.appspot.com/o/faculty%2Fcomps%2Fanilchhangani.jpg?alt=media&token=c1dcd2c0-cd00-4b00-aa9d-90481b179587";
+
+    case "Archana Patankar":
+      return "https://firebasestorage.googleapis.com/v0/b/tsec-app.appspot.com/o/faculty%2Fcomps%2Farchanapatankar.jpg?alt=media&token=e365c86b-14c8-4c05-bb13-4d4ec097ee23";
+
+    case "Arti Deshpande":
+      return "https://firebasestorage.googleapis.com/v0/b/tsec-app.appspot.com/o/faculty%2Fcomps%2Farchanapatankar.jpg?alt=media&token=e365c86b-14c8-4c05-bb13-4d4ec097ee23";
+
+    case "Darakshan Khan":
+      return "https://firebasestorage.googleapis.com/v0/b/tsec-app.appspot.com/o/faculty%2Fcomps%2Fdarakhshankhan.jpg?alt=media&token=18e920bc-a67c-4208-b838-45d9a7845a85";
+
+    case "Jayant Gadge":
+      return "https://firebasestorage.googleapis.com/v0/b/tsec-app.appspot.com/o/faculty%2Fcomps%2Fjayantgadge.jpg?alt=media&token=5ca4359f-219e-4f4d-8226-fa407c2510fd";
+
+    case "Juhi Ganwani":
+      return "https://firebasestorage.googleapis.com/v0/b/tsec-app.appspot.com/o/faculty%2Fcomps%2Fjuhiganwani.jpg?alt=media&token=595e3ec6-636a-4ad9-b03e-a393324f4d69";
+
+    case "Manisha Dumbre":
+      return "https://firebasestorage.googleapis.com/v0/b/tsec-app.appspot.com/o/faculty%2Fcomps%2Fmanishadumbre.jpg?alt=media&token=e2197a74-ebe0-4a10-9d70-73cad5842f58";
+
+    case "Nabanita Mandal":
+      return "https://firebasestorage.googleapis.com/v0/b/tsec-app.appspot.com/o/faculty%2Fcomps%2Fnabanitamandal.jpg?alt=media&token=42c3dccf-d61c-467e-873e-ba6aface4c1a";
+
+    case "Pallavi Halarnkar":
+      return "https://firebasestorage.googleapis.com/v0/b/tsec-app.appspot.com/o/faculty%2Fcomps%2Fpallavihalarnkar.jpg?alt=media&token=ad416225-f43e-4d5e-91f6-a7b23553d880";
+
+    case "Ruhi Bajaj":
+      return "https://firebasestorage.googleapis.com/v0/b/tsec-app.appspot.com/o/faculty%2Fcomps%2Fruhibajaj.jpg?alt=media&token=e41ef85b-27c9-44f6-b546-91501d3d100a";
+
+    case "Rupali Patil":
+      return "https://firebasestorage.googleapis.com/v0/b/tsec-app.appspot.com/o/faculty%2Fcomps%2Frupalipatil.jpg?alt=media&token=9110c496-ac93-4ca6-8a23-05c4787a8fcd";
+
+    case "Sakshi Surve":
+      return "https://firebasestorage.googleapis.com/v0/b/tsec-app.appspot.com/o/faculty%2Fcomps%2Fsakshisurve.jpg?alt=media&token=1ff5facd-e8e1-4802-a854-a24794addabc";
+
+    case "Seema Kolkur":
+      return "https://firebasestorage.googleapis.com/v0/b/tsec-app.appspot.com/o/faculty%2Fcomps%2Fseemakolkur.jpg?alt=media&token=606eb40d-69c1-40f0-8c11-37700ddd2007";
+
+    case "Shilpa Ingoley":
+      return "https://firebasestorage.googleapis.com/v0/b/tsec-app.appspot.com/o/faculty%2Fcomps%2Fshilpaingoley.jpg?alt=media&token=969ae091-1b36-4f4d-80be-c7b8a35d11ee";
+
+    case "Shilpa Verma":
+      return "https://firebasestorage.googleapis.com/v0/b/tsec-app.appspot.com/o/faculty%2Fcomps%2Fshilpaverma.jpg?alt=media&token=579ff33e-a4a9-4c99-91e9-e5276cdd35b8";
+
+    case "Sonal Shroff":
+      return "https://firebasestorage.googleapis.com/v0/b/tsec-app.appspot.com/o/faculty%2Fcomps%2Fsonalshroff.jpg?alt=media&token=6bd87dee-236f-475b-84ae-9d4c91d787a1";
+
+    case "Tanuja Sarode":
+      return "https://firebasestorage.googleapis.com/v0/b/tsec-app.appspot.com/o/faculty%2Fcomps%2Ftanujasarode.jpg?alt=media&token=60986cd7-d4a6-44ee-bc68-32823ba03c83";
+
+    case "Tasneem Mirza":
+      return "https://firebasestorage.googleapis.com/v0/b/tsec-app.appspot.com/o/faculty%2Fcomps%2Ftasneemmirza.jpg?alt=media&token=3c68fb48-cbfc-4fa7-a743-419787397b52";
+
+    case "Ujwala Bharambe":
+      return "https://firebasestorage.googleapis.com/v0/b/tsec-app.appspot.com/o/faculty%2Fcomps%2Fujwalabharambe.jpg?alt=media&token=8a736652-71a2-4de9-8a6f-5900ee5c50dd";
+
+    case "Urvi Kore":
+      return "https://firebasestorage.googleapis.com/v0/b/tsec-app.appspot.com/o/faculty%2Fcomps%2Furvikore.jpg?alt=media&token=120b3ae8-b11d-439a-96b6-7b2a1a704c73";
+
+    case "Vaibhav Ambhire":
+      return "https://firebasestorage.googleapis.com/v0/b/tsec-app.appspot.com/o/faculty%2Fcomps%2Fvaibhavambhire.jpg?alt=media&token=6211431a-553c-4919-8172-53cd2bd63432";
+
+    case "Vaishali Surywanshi":
+      return "https://firebasestorage.googleapis.com/v0/b/tsec-app.appspot.com/o/faculty%2Fcomps%2Fvaishalisuryawanshi.jpg?alt=media&token=eded9429-b622-4698-9ce3-107fe5ac4a45";
+
+    case "Vijaya Padmadas":
+      return "https://firebasestorage.googleapis.com/v0/b/tsec-app.appspot.com/o/faculty%2Fcomps%2Fvijayapadmadas.jpg?alt=media&token=4990452a-54a5-4c6a-b825-8bd5ab16bd83";
+
+    case "Anjali Malviya":
+      return "https://firebasestorage.googleapis.com/v0/b/tsec-app.appspot.com/o/faculty%2Fit%2Fanjalimalviya.jpg?alt=media&token=a4f6c351-5d30-4c28-ad66-dbc89fa6c21a";
+
+    case "Archana Kale":
+      return "https://firebasestorage.googleapis.com/v0/b/tsec-app.appspot.com/o/faculty%2Fit%2Farchanakale.jpg?alt=media&token=24db05eb-1bf6-45ee-8ba3-3ffd03a9445d";
+
+    case "Arun Kulkarni":
+      return "https://firebasestorage.googleapis.com/v0/b/tsec-app.appspot.com/o/faculty%2Fit%2Farunkulkarni.jpg?alt=media&token=eb09fee4-1764-4f4e-aa34-82a0c4b64520";
+
+    case "Bhushan Jadhav":
+      return "https://firebasestorage.googleapis.com/v0/b/tsec-app.appspot.com/o/faculty%2Fit%2Fbhushanjadhav.jpg?alt=media&token=28035d0d-9ada-4732-a512-f72dc8325bc3";
+
+    case "Chetan Agarwal":
+      return "https://firebasestorage.googleapis.com/v0/b/tsec-app.appspot.com/o/faculty%2Fit%2Fchetanagarwal.jpg?alt=media&token=be4146ae-3cd8-47ed-9c5e-ac0a80ec3b75";
+
+    case "Darshan Ingle":
+      return "https://firebasestorage.googleapis.com/v0/b/tsec-app.appspot.com/o/faculty%2Fit%2Fdarshaningle.jpg?alt=media&token=cdde18b7-6e1a-4250-83fb-f34971fc30df";
+
+    case "Geeta Karande":
+      return "https://firebasestorage.googleapis.com/v0/b/tsec-app.appspot.com/o/faculty%2Fit%2Fgeetakarande.jpg?alt=media&token=bf07fb42-40e7-4bff-9b3b-04e1787cfcc0";
+
+    case "Gopal Pardesi":
+      return "https://firebasestorage.googleapis.com/v0/b/tsec-app.appspot.com/o/faculty%2Fit%2Fgopalpardesi.jpg?alt=media&token=8aec2306-1469-4833-89c1-a99a9899bed3";
+
+    case "GT Thampi":
+      return "https://firebasestorage.googleapis.com/v0/b/tsec-app.appspot.com/o/faculty%2Fit%2Fgtthampi.jpg?alt=media&token=fc5ad5ee-f532-40f6-81d7-e6a32c99ea41";
+
+    case "Himani Deshpande":
+      return "https://firebasestorage.googleapis.com/v0/b/tsec-app.appspot.com/o/faculty%2Fit%2Fhimanideshpande.jpg?alt=media&token=79759228-9181-4159-9323-8900ee1d8275";
+
+    case "Kumkum Saxena":
+      return "https://firebasestorage.googleapis.com/v0/b/tsec-app.appspot.com/o/faculty%2Fit%2Fkumkumsaxena.jpg?alt=media&token=5d0dcdba-74c5-4e93-9954-265af51c0586";
+
+    case "Madhuri Rao":
+      return "https://firebasestorage.googleapis.com/v0/b/tsec-app.appspot.com/o/faculty%2Fit%2Fmadhurirao.jpg?alt=media&token=58ec8eb9-756a-4278-b3dc-11ae22de4c02";
+
+    case "Monica Tolani":
+      return "https://firebasestorage.googleapis.com/v0/b/tsec-app.appspot.com/o/faculty%2Fit%2Fmonicatolani.jpg?alt=media&token=7165e5db-c0b6-48a5-9909-d5bf5e865ce4";
+
+    case "Mukesh Israni":
+      return "https://firebasestorage.googleapis.com/v0/b/tsec-app.appspot.com/o/faculty%2Fit%2Fmukeshisrani.jpg?alt=media&token=7bc0460d-1da7-48d6-90a5-54b417afb13e";
+
+    case "Nagaveni Hebbar":
+      return "https://firebasestorage.googleapis.com/v0/b/tsec-app.appspot.com/o/faculty%2Fit%2Fnagavenihebbar.jpg?alt=media&token=26cc6c36-1352-490b-bda4-4082d9b6625e";
+
+    case "Nikhilesh Joshi":
+      return "https://firebasestorage.googleapis.com/v0/b/tsec-app.appspot.com/o/faculty%2Fit%2Fnikhileshjoshi.jpg?alt=media&token=d8f54570-198e-4b89-8e67-b65fbcbafbe7";
+
+    case "Reshma Malik":
+      return "https://firebasestorage.googleapis.com/v0/b/tsec-app.appspot.com/o/faculty%2Fit%2Freshmamalik.jpg?alt=media&token=9eab63e2-c510-489a-9ff5-3e1b2aa7a4c5";
+
+    case "Sampada Pinge":
+      return "https://firebasestorage.googleapis.com/v0/b/tsec-app.appspot.com/o/faculty%2Fit%2Fsampadapinge.jpg?alt=media&token=91064b98-1595-41de-b9ca-cb76dfbb4019";
+
+    case "Sanjay Pandey":
+      return "https://firebasestorage.googleapis.com/v0/b/tsec-app.appspot.com/o/faculty%2Fit%2Fsanjaypandey.jpg?alt=media&token=bfe69aeb-5fcc-49ca-a81f-8cb9d3efead2";
+
+    case "Sanober Shaikh":
+      return "https://firebasestorage.googleapis.com/v0/b/tsec-app.appspot.com/o/faculty%2Fit%2Fsanobershaikh.jpg?alt=media&token=cf288b35-f212-45dd-a1fe-d6f8355cebb2";
+
+    case "Shachi Natu":
+      return "https://firebasestorage.googleapis.com/v0/b/tsec-app.appspot.com/o/faculty%2Fit%2Fshachinatu.jpg?alt=media&token=fe274fe5-f5c6-4296-9bdf-1027c9708c33";
+
+    case "Shanthi Therese":
+      return "https://firebasestorage.googleapis.com/v0/b/tsec-app.appspot.com/o/faculty%2Fit%2Fshanthitherese.jpg?alt=media&token=751047c4-36b6-472d-ad0e-d045085ba799";
+
+    case "Sunita Rathod":
+      return "https://firebasestorage.googleapis.com/v0/b/tsec-app.appspot.com/o/faculty%2Fit%2Fsunitarathod.jpg?alt=media&token=6157a8a2-07e7-498b-bdc3-30fb995d316e";
+
+    case "Vijal Jain":
+      return "https://firebasestorage.googleapis.com/v0/b/tsec-app.appspot.com/o/faculty%2Fit%2Fvijaljain.jpg?alt=media&token=c7dc56b5-23c3-491b-9c4c-18665adbed14";
+
+    case "Bharati Ingale":
+      return "https://firebasestorage.googleapis.com/v0/b/tsec-app.appspot.com/o/faculty%2Faids%2Fbharati_ingale.jpg?alt=media&token=12ce7b39-7836-4556-bc0e-6c81dca39687";
+
+    case "Bhushan Jadhav":
+      return "https://firebasestorage.googleapis.com/v0/b/tsec-app.appspot.com/o/faculty%2Faids%2Fbhushan.jpg?alt=media&token=fbe34e2b-b5bd-439f-a67a-8882e4b81917";
+
+    case "Madhuri Rao":
+      return "https://firebasestorage.googleapis.com/v0/b/tsec-app.appspot.com/o/faculty%2Faids%2Fmadhuri-rao.png?alt=media&token=6174dbf7-78bf-4f36-a06f-1b7cc1768d4b";
+
+    case "Meenu Bhatia":
+      return "https://firebasestorage.googleapis.com/v0/b/tsec-app.appspot.com/o/faculty%2Faids%2Fmeenu_bhatia.jpg?alt=media&token=c555d65d-75e5-4330-aedd-5a360436933b";
+
+    case "Shirish Vichare":
+      return "https://firebasestorage.googleapis.com/v0/b/tsec-app.appspot.com/o/faculty%2Faids%2Fshirish_vichare.png?alt=media&token=d777ce1c-8bfd-43c8-96ff-c7001c8a9772";
+
+    case "Amit Thaker":
+      return "https://firebasestorage.googleapis.com/v0/b/tsec-app.appspot.com/o/faculty%2Fextc%2Famithatekar.jpg?alt=media&token=c066429a-78a1-4748-bd08-be30df7629ac";
+
+    case "Anuradha Rao":
+      return "https://firebasestorage.googleapis.com/v0/b/tsec-app.appspot.com/o/faculty%2Fextc%2Fanuradharao.jpg?alt=media&token=6c8e0c23-dc7c-42db-8182-43acbe955103";
+
+    case "Anushree Gupta":
+      return "https://firebasestorage.googleapis.com/v0/b/tsec-app.appspot.com/o/faculty%2Fextc%2Fanushreegupta.jpg?alt=media&token=71acde5a-ea06-4b24-9175-e6f986e41ec5";
+
+    case "Ashwini Kunte":
+      return "https://firebasestorage.googleapis.com/v0/b/tsec-app.appspot.com/o/faculty%2Fextc%2Fashwinikunte.jpg?alt=media&token=ccd5eedd-c57c-4163-a565-1f80b59d5cb6";
+
+    case "Jyoti Kashyap":
+      return "https://firebasestorage.googleapis.com/v0/b/tsec-app.appspot.com/o/faculty%2Fextc%2Fjyotikashyap.jpg?alt=media&token=0ff79afb-7b1e-4783-a786-c4800178e8ac";
+
+    case "KK Mathew":
+      return "https://firebasestorage.googleapis.com/v0/b/tsec-app.appspot.com/o/faculty%2Fextc%2Fkkmathew.jpg?alt=media&token=af2c8b84-d723-4b93-8ab2-b672290633dd";
+
+    case "KY Rajput":
+      return "https://firebasestorage.googleapis.com/v0/b/tsec-app.appspot.com/o/faculty%2Fextc%2Fkyrajput.jpg?alt=media&token=f5e9f933-7b5b-46f8-84ac-0efdeeceb12e";
+
+    case "Maniroja Edinburgh":
+      return "https://firebasestorage.googleapis.com/v0/b/tsec-app.appspot.com/o/faculty%2Fextc%2Fmanirojaedinburgh.jpg?alt=media&token=2c1c212d-27d2-49f7-9ce3-6cc2397de559";
+
+    case "Manoj Kavedia":
+      return "https://firebasestorage.googleapis.com/v0/b/tsec-app.appspot.com/o/faculty%2Fextc%2Fmanojkavedia.jpg?alt=media&token=341635fc-8c82-4303-9f0b-dc9f46da4c80";
+
+    case "Medha Solamwar":
+      return "https://firebasestorage.googleapis.com/v0/b/tsec-app.appspot.com/o/faculty%2Fextc%2Fmedhasomalwar.jpg?alt=media&token=f4b2b8ff-33b6-4d70-af70-e020d941a057";
+
+    case "Neeru Pathak":
+      return "https://firebasestorage.googleapis.com/v0/b/tsec-app.appspot.com/o/faculty%2Fextc%2Fneerupathak.jpg?alt=media&token=321486c1-7ca4-471e-a418-ad598d43c768";
+
+    case "Sharmila Barve":
+      return "https://firebasestorage.googleapis.com/v0/b/tsec-app.appspot.com/o/faculty%2Fextc%2Fsharmilabarve.jpg?alt=media&token=36bf899d-76e1-4e6c-88cd-dbee515d94f9";
+
+    case "Shristi Bhatia":
+      return "https://firebasestorage.googleapis.com/v0/b/tsec-app.appspot.com/o/faculty%2Fextc%2Fshristibhatia.jpg?alt=media&token=fa655b0a-da86-4f91-9a00-0328cd46748b";
+
+    case "Uttara Bhatt":
+      return "https://firebasestorage.googleapis.com/v0/b/tsec-app.appspot.com/o/faculty%2Fextc%2Futtarabhatt.jpg?alt=media&token=d8cda8a3-2e61-41f3-abdb-02f4efefb0a8";
+
+    case "Vijayalakshmi Barve":
+      return "https://firebasestorage.googleapis.com/v0/b/tsec-app.appspot.com/o/faculty%2Fextc%2Fvijayalakshmibadre.jpg?alt=media&token=9cdaa433-d699-4ffb-bd38-937a31facbe7";
+
+    case "Anita Kumari":
+      return "https://firebasestorage.googleapis.com/v0/b/tsec-app.appspot.com/o/faculty%2Fchem%2Fanitakumari.jpg?alt=media&token=f59271ab-1e9c-4473-b7ad-e8483ba164a7";
+
+    case "Elizabeth Joseph":
+      return "https://firebasestorage.googleapis.com/v0/b/tsec-app.appspot.com/o/faculty%2Fchem%2Felizabethjoseph.jpg?alt=media&token=8cb20441-661e-478c-b9f6-291ce2551ba5";
+
+    case "Karuna Saswani":
+      return "https://firebasestorage.googleapis.com/v0/b/tsec-app.appspot.com/o/faculty%2Fchem%2Fkarunasaswani.jpg?alt=media&token=30239d8a-fd04-4c98-a917-8349c1bfb2dd";
+
+    case "Lakshay Girdhar":
+      return "https://firebasestorage.googleapis.com/v0/b/tsec-app.appspot.com/o/faculty%2Fchem%2Flakshaygirdhar.jpg?alt=media&token=c348cbde-e192-4171-82fa-d1ef6a2240d1";
+
+    case "Nita Mehta":
+      return "https://firebasestorage.googleapis.com/v0/b/tsec-app.appspot.com/o/faculty%2Fchem%2Fnitamehta.jpg?alt=media&token=387cd678-67fd-4272-ad11-9516b3c44476";
+
+    case "Prasad Jayavant Parulekar":
+      return "https://firebasestorage.googleapis.com/v0/b/tsec-app.appspot.com/o/faculty%2Fchem%2Fprasadjayavantparulekar.jpg?alt=media&token=142ae663-22d8-4747-8077-a5a0fb757a87";
+
+    case "Ramesh Bhande":
+      return "https://firebasestorage.googleapis.com/v0/b/tsec-app.appspot.com/o/faculty%2Fchem%2Frameshbhande.jpg?alt=media&token=c572b793-ed22-4dc0-9fac-17ac6975018c";
+
+    case "RK Pathak":
+      return "https://firebasestorage.googleapis.com/v0/b/tsec-app.appspot.com/o/faculty%2Fchem%2Frkpathak.jpg?alt=media&token=7ea58730-f38b-45f1-9c53-ffc51e113b21";
+
+    case "RR Joshi":
+      return "https://firebasestorage.googleapis.com/v0/b/tsec-app.appspot.com/o/faculty%2Fchem%2Frrjoshi.png?alt=media&token=fb73c422-c741-4c71-b25a-4767c5fdabd8";
+
+    case "Sadhana Purohit":
+      return "https://firebasestorage.googleapis.com/v0/b/tsec-app.appspot.com/o/faculty%2Fchem%2Fsadhanapurohit.jpg?alt=media&token=6b4ceb0e-072d-4d37-9c77-397797d96bdf";
+
+    case "Sangita Borkar":
+      return "https://firebasestorage.googleapis.com/v0/b/tsec-app.appspot.com/o/faculty%2Fchem%2Fsangitaborkar.jpg?alt=media&token=cdbe7b19-ab03-4892-9d33-32d729bb0a7d";
+
+    case "Trupti Parag Dharmarao":
+      return "https://firebasestorage.googleapis.com/v0/b/tsec-app.appspot.com/o/faculty%2Fchem%2Ftruptiparagdharmarao.jpg?alt=media&token=a720addb-fe4d-436a-88cc-db11f511dd59";
+
+    case "Usha Nambiar":
+      return "https://firebasestorage.googleapis.com/v0/b/tsec-app.appspot.com/o/faculty%2Fchem%2Fushanambiar.jpg?alt=media&token=c2308154-170a-443e-a75c-1f0ab4c33507";
+
+    case "Ganesh Tilave":
+      return "https://firebasestorage.googleapis.com/v0/b/tsec-app.appspot.com/o/faculty%2Fother%2Fmaths%2Fganeshtilave.jpg?alt=media&token=92b39364-f378-4f09-a2c5-73018e09bced";
+
+    case "Gaurav Mittal":
+      return "https://firebasestorage.googleapis.com/v0/b/tsec-app.appspot.com/o/faculty%2Fother%2Fmaths%2Fgauravmittal.jpg?alt=media&token=fad0e3f1-8820-4a01-b6e7-f9e1ee5a2f8b";
+
+    case "Mansha Kotwani":
+      return "https://firebasestorage.googleapis.com/v0/b/tsec-app.appspot.com/o/faculty%2Fother%2Fmaths%2Fmanshakotwani.png?alt=media&token=b0b5c07a-87eb-4fe9-9211-07456345b79e";
+
+    case "Poonam Soni":
+      return "https://firebasestorage.googleapis.com/v0/b/tsec-app.appspot.com/o/faculty%2Fother%2Fmaths%2Fpoonamsoni.jpg?alt=media&token=3700ec8c-99cd-45a3-b78e-8746d082231a";
+
+    case "Sabatini Almeida":
+      return "https://firebasestorage.googleapis.com/v0/b/tsec-app.appspot.com/o/faculty%2Fother%2Fmaths%2Fsabatinialmeida.jpg?alt=media&token=9b88cdd9-cfe6-4d12-9bf4-ef6d3d5af574";
+
+    case "Saket Kumar":
+      return "https://firebasestorage.googleapis.com/v0/b/tsec-app.appspot.com/o/faculty%2Fother%2Fmaths%2Fsaketkumar.jpg?alt=media&token=c6357278-8848-4358-8648-459c568b8dfa";
+
+    case "Iqbal Hussain":
+      return "https://firebasestorage.googleapis.com/v0/b/tsec-app.appspot.com/o/faculty%2Fother%2Fmech%2Fiqbalhusain.png?alt=media&token=d01ed0d8-3aac-4917-8b4d-3c6ae59bc27d";
+
+    case "Kiran Ghadyalji":
+      return "https://firebasestorage.googleapis.com/v0/b/tsec-app.appspot.com/o/faculty%2Fother%2Fmech%2Fkiranghadyalji.jpg?alt=media&token=caadfadb-7922-4e7a-bbce-3b4861f9b3e9";
+
+    case "Mahesh Rao":
+      return "https://firebasestorage.googleapis.com/v0/b/tsec-app.appspot.com/o/faculty%2Fother%2Fmech%2Fmaheshrao.png?alt=media&token=a16d6655-278e-45a9-a3c5-13b2e24db00d";
+
+    case "Mudita Dixit":
+      return "https://firebasestorage.googleapis.com/v0/b/tsec-app.appspot.com/o/faculty%2Fother%2Fmech%2Fmuditadixit.png?alt=media&token=18d888b6-c622-4b2b-a4fe-b82dcb9c4986";
+
+    case "Sachit Nalaskar":
+      return "https://firebasestorage.googleapis.com/v0/b/tsec-app.appspot.com/o/faculty%2Fother%2Fmech%2Fsachitnalaskar.jpg?alt=media&token=aaf11c2a-c923-41dc-a42e-990e1cf78a1e";
+
+    case "Umesh Kulal":
+      return "https://firebasestorage.googleapis.com/v0/b/tsec-app.appspot.com/o/faculty%2Fother%2Fmech%2Fumeshkulal.jpg?alt=media&token=aa643828-045e-4b24-a81b-581e68f6af3b";
+
+    default:
+      return "https://firebasestorage.googleapis.com/v0/b/tsec-app.appspot.com/o/faculty%2Fprofile_image_placeholder.png?alt=media&token=64982c42-2a39-440b-a23c-712d9a9eefae";
+  }
+}
