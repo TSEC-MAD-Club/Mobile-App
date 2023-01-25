127a03ec04ba4d78fbcab6c553e1a0a5ff26eedb
AbhinayPatil <10abhinay10@gmail.com>
2023-01-25 21:32:10 +0530

    add teacher details and committee details util file

---
+++ lib/utils/committees_details.dart
@@ -0,0 +1,8 @@
+String getCommitteeImagebyName(String committeeName) {
+  switch (committeeName) {
+    case "Tsec Codecell":
+      return " ";
+    default:
+      return ""; 
+  }
+}
---
+++ lib/utils/teachers_details.dart
@@ -0,0 +1,8 @@
+String getTeacherImagebyName(String facultyName) {
+  switch (facultyName) {
+    case "Darakhshan Khan":
+      return " ";
+    default:
+      return ""; 
+  }
+}
