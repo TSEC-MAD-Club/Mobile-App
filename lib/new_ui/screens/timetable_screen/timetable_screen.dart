b285decea588813246be580c5142eca7c5f68591
devout-coder <awessssomepro@gmail.com>
2024-02-28 20:57:08 +0530

    super minor UI fixes

--- lib/new_ui/screens/timetable_screen/timetable_screen.dart
+++ lib/new_ui/screens/timetable_screen/timetable_screen.dart
@@ -108,7 +108,7 @@ class TimeTable extends ConsumerWidget {
             Padding(
               padding: const EdgeInsets.fromLTRB(20, 10, 20, 10),
               child: Container(
-                  height: MediaQuery.of(context).size.height * .55,
+                  height: MediaQuery.of(context).size.height * 0.5,
                   width: MediaQuery.of(context).size.width * 0.9,
                   decoration: BoxDecoration(
                     color: _theme.colorScheme.tertiary,
