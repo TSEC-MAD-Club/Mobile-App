91e768d4b0a7aa4dac927ae4bb13d483e72111e9
Aryan <aupathak1234@gmail.com>
2024-06-11 23:19:56 +0530

    Improved the logic of the bottom nav bar. Please check if it works with a faculty account once before merging.

--- lib/new_ui/colors.dart
+++ lib/new_ui/colors.dart
@@ -1,5 +1,11 @@
 import 'dart:ui';
 
+import 'package:flutter/material.dart';
+
 const commonbgblack = Color(0xff18191b);
 const cardcolorblue  = Color(0xff0077b6);
 const cardlightblue = Color(0xff90e0ef);
+// const commonbgwhite = Color(0xfff1f5f8);
+const greencontainercolor = Color(0xff2e9d59);
+//the red container uses a gradient, unsure of its start and end values
+const darkTextFieldBgColor = Color(0xff323232);
