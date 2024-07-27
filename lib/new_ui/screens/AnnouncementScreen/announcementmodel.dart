1165bc203c6289a1824966294ddcc8af3dbcd50f
SayedZeeshanHyder <zhyder133@gmail.com>
2024-07-28 00:50:18 +0530

    Added Announement Screen UI

--- lib/new_ui/screens/AnnouncementScreen/announcementmodel.dart
+++ lib/new_ui/screens/AnnouncementScreen/announcementmodel.dart
@@ -3,29 +3,35 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 class AnnouncementModel {
   Timestamp? startDate;
   Timestamp? endDate;
+  String? content;
   String? div;
   String? branch;
   String? batch;
   String? docURL;
   String? title;
+  String? gradYear;
 
   AnnouncementModel(
       {this.startDate,
+        this.content,
         this.endDate,
         this.div,
         this.branch,
         this.batch,
         this.docURL,
+        this.gradYear,
         this.title});
 
   AnnouncementModel.fromJson(Map<String, dynamic> json) {
     startDate = json['startDate'];
+    content = json['content'];
     endDate = json['endDate'];
     div = json['div'];
     branch = json['branch'];
     batch = json['batch'];
     docURL = json['docURL'];
     title = json['title'];
+    gradYear = json["gradYear"];
   }
 
   Map<String, dynamic> toJson() {
@@ -33,10 +39,12 @@ class AnnouncementModel {
     data['startDate'] = this.startDate;
     data['endDate'] = this.endDate;
     data['div'] = this.div;
+    data['content'] = this.content;
     data['branch'] = this.branch;
     data['batch'] = this.batch;
     data['docURL'] = this.docURL;
     data['title'] = this.title;
+    data['gradYear'] = this.gradYear;
     return data;
   }
 }
