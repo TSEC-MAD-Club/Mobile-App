d5ac3b7cdf29716c4070597479974fe7cd075eec
Manish R <manishraja2505@gmail.com>
2026-07-01 13:06:21 +0530

    impl : very intial ios widget file structure

---
+++ ios/TimeTableWidget/TimeTableWidgetControl.swift
@@ -0,0 +1,54 @@
+//
+//  TimeTableWidgetControl.swift
+//  TimeTableWidget
+//
+//  Created by Manish Rajarathinam on 30/06/26.
+//
+
+import AppIntents
+import SwiftUI
+import WidgetKit
+
+struct TimeTableWidgetControl: ControlWidget {
+    var body: some ControlWidgetConfiguration {
+        StaticControlConfiguration(
+            kind: "com.devsclub.TimeTableWidget",
+            provider: Provider()
+        ) { value in
+            ControlWidgetToggle(
+                "Start Timer",
+                isOn: value,
+                action: StartTimerIntent()
+            ) { isRunning in
+                Label(isRunning ? "On" : "Off", systemImage: "timer")
+            }
+        }
+        .displayName("Timer")
+        .description("A an example control that runs a timer.")
+    }
+}
+
+extension TimeTableWidgetControl {
+    struct Provider: ControlValueProvider {
+        var previewValue: Bool {
+            false
+        }
+
+        func currentValue() async throws -> Bool {
+            let isRunning = true // Check if the timer is running
+            return isRunning
+        }
+    }
+}
+
+struct StartTimerIntent: SetValueIntent {
+    static let title: LocalizedStringResource = "Start a timer"
+
+    @Parameter(title: "Timer is running")
+    var value: Bool
+
+    func perform() async throws -> some IntentResult {
+        // Start / stop the timer based on `value`.
+        return .result()
+    }
+}
